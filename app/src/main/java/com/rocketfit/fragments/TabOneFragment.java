74c6dc4d470c870efb815cda3c3b83880a9f4990
david <dhritz@rockets.utoledo.edu>
2014-12-03 11:10:17 -0500

    ... Used ArrayList instead of Array. Too easy. No other changes.

--- app/src/main/java/com/rocketfit/fragments/TabOneFragment.java
+++ app/src/main/java/com/rocketfit/fragments/TabOneFragment.java
@@ -1,54 +1,42 @@
 package com.rocketfit.fragments;
 
-import android.app.ActionBar;
-import android.app.Activity;
 import android.app.AlertDialog;
-import android.app.ProgressDialog;
 import android.content.DialogInterface;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.text.Html;
-import android.util.Log;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.support.v4.app.Fragment;
-import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.ProgressBar;
-import android.widget.TextView;
-import android.widget.Toast;
 
 
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
-import com.parse.ParseRelation;
 import com.parse.ParseUser;
-import com.rocketfit.activities.HomeActivity;
-import com.rocketfit.activities.ProfileActivity;
-import com.rocketfit.activities.WeightsActivity;
 
-import java.util.Date;
+import java.util.ArrayList;
 import java.util.List;
 
 import projects.rocketfit.R;
 
+
+
 public class TabOneFragment extends android.support.v4.app.Fragment {
+    public static int NUMBER_OF_RECENT_WORKOUTS = 5;
     private ListView recentWorkouts;
     private View recentItems;
-    public StringBuilder[] wSum = new StringBuilder[5];
-    private ProgressBar mLoading;
 
+    ArrayList<StringBuilder> wSum = new ArrayList<StringBuilder>();
+    ArrayList<String> values = new ArrayList<String>();
+
+    private ProgressBar mLoading;
     // Defined Array values to show in ListView
-    private String[] values = new String[5];
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -74,16 +62,9 @@ public class TabOneFragment extends android.support.v4.app.Fragment {
             mLoading = (ProgressBar) recentItems.findViewById(R.id.progressbar_loading);
             mLoading.setVisibility(View.VISIBLE);
 
-            // create a progress dialog
-//            mLoading = new ProgressDialog(getActivity());
-//            mLoading.setMessage("Loading Data...");
-//            mLoading.setTitle("");
-//            mLoading.show();
-
             // Handler is called when thread is complete that sends data to parse below
             final Handler handler = new Handler() {
                 public void handleMessage(Message msg) {
-//                    mLoading.dismiss();  // dismiss the dialog
                     mLoading.setVisibility(View.GONE);
 
                     // Get ListView object from xml
@@ -93,6 +74,8 @@ public class TabOneFragment extends android.support.v4.app.Fragment {
                     // Second parameter - Layout for the row
                     // Third parameter - ID of the TextView to which the data is written
                     // Forth - the Array of data
+
+                  //  ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity().getApplicationContext(), R.layout.custom, R.id.listTextView, values);
                     ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity().getApplicationContext(), R.layout.custom, R.id.listTextView, values);
 
                     // Assign adapter to ListView
@@ -113,7 +96,7 @@ public class TabOneFragment extends android.support.v4.app.Fragment {
 
                             AlertDialog.Builder workoutSummary = new AlertDialog.Builder(getActivity());
                             workoutSummary.setTitle("Your Workout Summary");
-                            workoutSummary.setMessage(Html.fromHtml(wSum[itemPosition].toString()));
+                            workoutSummary.setMessage(Html.fromHtml(wSum.get(itemPosition).toString()));
                             workoutSummary.setCancelable(true);
                             workoutSummary.setPositiveButton("Close Summary",
                                     new DialogInterface.OnClickListener() {
@@ -139,15 +122,17 @@ public class TabOneFragment extends android.support.v4.app.Fragment {
 
                     ParseQuery<ParseObject> workoutQuery = currentUser.getRelation("workouts").getQuery();
                     workoutQuery.orderByDescending("createdAt");
-                    workoutQuery.setLimit(5);
+                    workoutQuery.setLimit(NUMBER_OF_RECENT_WORKOUTS);
                     try {
                         List<ParseObject> wObjects = workoutQuery.find();
 
                         for (int i = 0; i < wObjects.size(); i++) {
                             ParseObject oneWorkout = wObjects.get(i);
                             String createdAt = oneWorkout.getCreatedAt().toString();
-                            values[i] = "    Workout on " + createdAt;
-                            wSum[i] = new StringBuilder();
+                            String message = "    Workout on " + createdAt;
+
+                            wSum.add(i, new StringBuilder());
+                            values.add(i, message);
 
                             ParseQuery<ParseObject> setsQuery = oneWorkout.getRelation("sets").getQuery();
                             setsQuery.orderByAscending("createdAt");
@@ -165,17 +150,17 @@ public class TabOneFragment extends android.support.v4.app.Fragment {
                                         strMachine = machine.get("name").toString();
 
                                         if (k == 0) {
-                                            wSum[i].append(String.format("<b>%s</b>", strMachine) + "<br>");
+                                            wSum.get(i).append(String.format("<b>%s</b>", strMachine) + "<br>");
                                         } else if (!(lastMachine.compareTo(strMachine) == 0)) {
-                                            wSum[i].append(String.format("<br>" + "<b>%s</b>", strMachine) + "<br>");
+                                            wSum.get(i).append(String.format("<b>%s</b>", strMachine) + "<br>");
                                         }
 
                                     } catch (ParseException e1) {
 
                                     }
 
-                                    wSum[i].append("Reps: " + oneSet.get("repetitions").toString() + "&nbsp;&nbsp;&nbsp;&nbsp;");
-                                    wSum[i].append("Weight: " + oneSet.get("resistance").toString() + " lbs" + "<br>");
+                                    wSum.get(i).append("Reps: " + oneSet.get("repetitions").toString() + "&nbsp;&nbsp;&nbsp;&nbsp;");
+                                    wSum.get(i).append("Weight: " + oneSet.get("resistance").toString() + " lbs" + "<br>");
 
                                     lastMachine = strMachine;
                                 }
@@ -184,6 +169,9 @@ public class TabOneFragment extends android.support.v4.app.Fragment {
                             }
                         }
 
+                        if(wObjects.size() <= 5) {
+
+                        }
                     } catch (ParseException eWorkout) {
 
                     }
